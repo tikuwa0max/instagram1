d090c6b2116e356230829995fe7341783e6487f7
市澤樹享 <tikuwa0max@ichizawajutoorunoMacBook-Air.local>
2019-01-04 09:06:35 +0900

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by 市澤樹享 on 2019/01/04.
+//  Copyright © 2019 mikitaka.ichizawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
