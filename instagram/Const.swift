2a442f2e64b92760048b6bcd5daa400c4e382975
市澤樹享 <tikuwa0max@ichizawajutoorunoMacBook-Air.local>
2019-01-09 16:38:53 +0900

    課題前まで

---
+++ instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  instagram
+//
+//  Created by 市澤樹享 on 2019/01/09.
+//  Copyright © 2019 mikitaka.ichizawa. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
---
+++ instagram/PostData.swift
@@ -0,0 +1,48 @@
+//
+//  PostData.swift
+//  instagram
+//
+//  Created by 市澤樹享 on 2019/01/09.
+//  Copyright © 2019 mikitaka.ichizawa. All rights reserved.
+//
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class PostData: NSObject {
+    var id: String?
+    var image: UIImage?
+    var imageString: String?
+    var name: String?
+    var caption: String?
+    var date: Date?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    
+    init(snapshot: DataSnapshot, myId: String) {
+        self.id = snapshot.key
+        
+        let valueDictionary = snapshot.value as! [String: Any]
+        
+        imageString = valueDictionary["image"] as? String
+        image = UIImage(data: Data(base64Encoded: imageString!, options: .ignoreUnknownCharacters)!)
+        
+        self.name = valueDictionary["name"] as? String
+        
+        self.caption = valueDictionary["caption"] as? String
+        
+        let time = valueDictionary["time"] as? String
+        self.date = Date(timeIntervalSinceReferenceDate: TimeInterval(time!)!)
+        
+        if let likes = valueDictionary["likes"] as? [String] {
+            self.likes = likes
+        }
+        
+        for likeId in self.likes {
+            if likeId == myId {
+                self.isLiked = true
+                break
+            }
+        }
+    }
+}
